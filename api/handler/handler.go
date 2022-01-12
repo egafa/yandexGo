9a55e1f06371f59ade13bad88ed36fe774818a35
egafa <48982337+egafa@users.noreply.github.com>
2022-01-12 21:08:57 +0300

    handlers

--- api/handler/handler.go
+++ api/handler/handler.go
@@ -58,17 +58,10 @@ func UpdateMetricHandlerChi(m model.Metric) http.HandlerFunc {
 func ValueMetricHandlerChi(m model.Metric) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 
-		act := chi.URLParam(r, "act")
-		if act != "value" {
-			w.WriteHeader(http.StatusBadRequest)
-			http.Error(w, "Не определена метрика", http.StatusBadRequest)
-			return
-		}
-
 		typeMetric := chi.URLParam(r, "typeMetric")
 		nameMetric := chi.URLParam(r, "nammeMetric")
 
-		switch typeMetric {
+		switch strings.ToLower(typeMetric) {
 		case "gauge":
 			val, ok := m.GetGaugeVal(nameMetric)
 			if ok {
