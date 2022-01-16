eafca9fc8b45e277c1825972e41ddad7bb738e05
egafa <48982337+egafa@users.noreply.github.com>
2022-01-16 20:23:28 +0300

    test tests

--- cmd/agent/main.go
+++ cmd/agent/main.go
@@ -157,7 +157,7 @@ func main() {
 
 	go formMetric(ctx, cfg, namesMetric, dataChannel)
 
-	timer := time.NewTimer(4 * time.Second) // создаём таймер
+	timer := time.NewTimer(2 * time.Second)
 	<-timer.C
 
 	go sendMetric(ctx, dataChannel, stopchanel, cfg)
@@ -165,7 +165,6 @@ func main() {
 	c := make(chan os.Signal, 1)
 	signal.Notify(c, os.Interrupt, syscall.SIGINT, syscall.SIGTERM, syscall.SIGQUIT)
 	// Block until a signal is received.
-
 	<-c
 
 	cancel()
